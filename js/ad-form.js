85c5e51084f4877876a4da3683cee84d1205a85f
Valeriy Kokorev <kokorev.valery@gmail.com>
2021-03-23 00:22:41 +0700

    Реализовал валидацию формы объявления.

--- js/ad-form.js
+++ js/ad-form.js
@@ -4,6 +4,25 @@ import {HOUSING_TYPES} from './data/references.js'
 
 const COORDS_DIGITS = 5
 
+const ROOMS_CAPACITY = {
+  1: {
+    capacity: ['1'],
+    errorMessage: 'В одной комнате может жить только 1 гость',
+  },
+  2: {
+    capacity: ['1', '2'],
+    errorMessage: 'В двух комнатах могут жить 1 или 2 гостя',
+  },
+  3: {
+    capacity: ['1', '2', '3'],
+    errorMessage: 'В трёх комнатах могут жить от 1 до 3 гостей',
+  },
+  100: {
+    capacity: ['0'],
+    errorMessage: '100 комнат не для гостей ',
+  },
+}
+
 /**
  * class for managing the behavior of the ad form.
  * @constructor
@@ -15,6 +34,9 @@ const AdForm = function () {
   const timeInInput = form.querySelector('#timein');
   const timeOutInput = form.querySelector('#timeout');
   const addressInput = form.querySelector('#address');
+  const titleInput = form.querySelector('#title');
+  const roomNumberInput = form.querySelector('#room_number');
+  const capacityInput = form.querySelector('#capacity');
   const clearButton = form.querySelector('.ad-form__reset');
 
   addressInput.readOnly = true;
@@ -76,6 +98,61 @@ const AdForm = function () {
   clearButton.addEventListener('click', () => {
     this.clear();
   });
+
+  titleInput.addEventListener('invalid', () => {
+    if (titleInput.validity.tooShort) {
+      titleInput.setCustomValidity('Заголовок не может быть короче 30-ти символов');
+    } else if (titleInput.validity.tooLong) {
+      titleInput.setCustomValidity('Заголовок не должен быть длинее 100 символов');
+    } else if (titleInput.validity.valueMissing) {
+      titleInput.setCustomValidity('Заголовок обязателен для заполнения');
+    } else {
+      titleInput.setCustomValidity('');
+    }
+  });
+
+  priceInput.addEventListener('invalid', () => {
+    if (priceInput.validity.rangeUnderflow) {
+      priceInput.setCustomValidity(`Цена за ночь не может быть меньше ${priceInput.min} рублей`);
+    } else if (priceInput.validity.rangeOverflow) {
+      priceInput.setCustomValidity('Цена за ночь не может быть больше 1000000 рублей');
+    } else if (priceInput.validity.valueMissing) {
+      priceInput.setCustomValidity('Цена за ночь обязателена для заполнения');
+    } else {
+      priceInput.setCustomValidity('');
+    }
+  });
+
+  const capacityValidate = () => {
+    const rooms = roomNumberInput.value;
+    const capacity = capacityInput.value;
+
+    let isValid = true;
+    if (!ROOMS_CAPACITY[rooms].capacity.includes(capacity)) {
+      capacityInput.setCustomValidity(ROOMS_CAPACITY[rooms].errorMessage);
+      isValid = false;
+    } else {
+      capacityInput.setCustomValidity('');
+    }
+    capacityInput.reportValidity();
+
+    return isValid;
+  };
+
+  roomNumberInput.addEventListener('change', () => {
+    capacityValidate();
+  });
+
+  capacityInput.addEventListener('change', () => {
+    capacityValidate();
+  });
+
+  form.addEventListener('submit', (evt) => {
+    if (!capacityValidate()) {
+      evt.preventDefault();
+    }
+  });
+
 }
 
 export {AdForm};
